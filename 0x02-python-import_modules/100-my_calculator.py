1a6d8e03f9ead039d307791279cf5d278899ab18
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-07-02 03:30:29 -0700

    Updated

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-import calculator_1 as calculate
-import sys
+if __name__ == "__main__"
+    import calculator_1 as calculate
+    import sys
 
-argc = len(sys.argv)
-if argc != 4:
-    print("Usage: >/100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    argc = len(sys.argv)
+    if argc != 4:
+        print("Usage: >/100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-a = int(sys.argv[1])
-b = int(sys.argv[3])
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
 
-if sys.argv[2] == '+':
-    c = calculate.add(a, b)
-elif sys.argv[2] == '-':
-    c = calculate.sub(a, b)
-elif sys.argv[2] == '*':
-    c = calculate.mul(a, b)
-elif sys.argv[2] == '/':
-    c = calculate.div(a, b)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    if sys.argv[2] == '+':
+        c = calculate.add(a, b)
+    elif sys.argv[2] == '-':
+        c = calculate.sub(a, b)
+    elif sys.argv[2] == '*':
+        c = calculate.mul(a, b)
+    elif sys.argv[2] == '/':
+        c = calculate.div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-print("{} {} {} = {}".format(a, sys.argv[2], b, c))
-sys.exit(0)
+    print("{} {} {} = {}".format(a, sys.argv[2], b, c))
+    sys.exit(0)
