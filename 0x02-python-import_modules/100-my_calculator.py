27a7859d54f0c458fb4e26f431e34be1f5315c6a
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-07-02 02:53:58 -0700

    Updated

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import calculator_1 as calculate
+import sys
+
+argc = len(sys.argv)
+if argc != 4:
+    print("Usage: >/100-my_calculator.py <a> <operator> <b>")
+    print("")
+    exit(1)
+
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+
+if sys.argv[2] == '+':
+    c = calculate.add(a, b)
+elif sys.argv[2] == '-':
+    c = calculate.sub(a, b)
+elif sys.argv[2] == '*':
+    c = calculate.mul(a, b)
+elif sys.argv[2] == '/':
+    c = calculate.div(a, b)
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    print("")
+    exit(1)
+
+print("{} {} {} = {}".format(a, sys.argv[2], b, c))
+print('')
