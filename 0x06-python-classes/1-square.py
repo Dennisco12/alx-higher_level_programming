f98c52626e7490a47d5a8950c6b0438491ae0db5
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-07-19 21:14:34 +0100

    updated

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,7 +6,6 @@
 class Square:
     '''Defines a class'''
 
-
     def __init__(self, size):
         '''Size is a private attribute that is crucial for a square
         and the type and value must be controlled'''
