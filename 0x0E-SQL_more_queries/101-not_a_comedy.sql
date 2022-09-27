a00246690fb9885151eba394d5277285c2c3b666
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-09-27 04:02:34 -0700

    Correcte task 101

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -6,7 +6,7 @@ LEFT JOIN (
 	SELECT tv_genres.id, tv_genres.name
 	FROM tv_genres
 	LEFT JOIN tv_shows_genres
-	ON tv_genres.id = tv_shows_genres.genre_id
+	ON tv_genres.id = tv_show_genres.genre_id
 	LEFT JOIN tv_shows
 	ON tv_show_genres.show_id = tv_shows.id
 	WHERE tv_genres.name = 'Comedy'
