178ef472b4f5a5c2998dbd600f0ce0e0265ac194
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-09-27 05:48:18 -0700

    Corrected task 101 again

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -3,14 +3,14 @@
 SELECT tv_shows.title
 FROM tv_shows
 LEFT JOIN (
-	SELECT tv_genres.id, tv_genres.name
-	FROM tv_genres
+	SELECT tv_shows.title
+	FROM tv_shows
 	LEFT JOIN tv_show_genres
-	ON tv_genres.id = tv_show_genres.genre_id
-	LEFT JOIN tv_shows
-	ON tv_show_genres.show_id = tv_shows.id
+	ON tv_shows.id = tv_show_genres.show_id
+	LEFT JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres.id
 	WHERE tv_genres.name = 'Comedy'
-) AS comedy_id
-ON comedy_id.title = tv_shows.title
-WHERE comedy_id.title = NULL
+) AS comedy_shows
+ON comedy_shows.title = tv_shows.title
+WHERE comedy_shows.title = NULL
 ORDER BY tv_shows.title ASC
