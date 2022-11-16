db8eaf8d17c784ec688f29c80cb8639198d5a181
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-11-15 23:24:25 -0800

    Updated semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,12 +2,12 @@
 
 const request = require('request');
 let endPoint = 'https://swapi-api.hbtn.io/api/films/';
-const id = process.argv[2]
+const id = process.argv[2];
 
-endPoint = endPoint + id
+endPoint = endPoint + id;
 request(endPoint, function (error, response, body) {
-	if (error) {
-		console.log(error);
-	} const content = JSON.parse(body)
-	console.log(content.title)
+  if (error) {
+    console.log(error);
+  } const content = JSON.parse(body);
+  console.log(content.title);
 });
